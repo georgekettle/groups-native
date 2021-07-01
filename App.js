92988c84240c957086731ee0aa4b5c8da103c4f3
George Kettle <georgekettle.gk@gmail.com>
2021-07-01 14:43:52 +1000

    initial setup done with changing of header

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+import { WebView } from 'react-native-webview';
+
+const INJECTED_JAVASCRIPT = `
+      window.isNativeApp = true;
+      window.statusBarHeight = ${Constants.statusBarHeight};
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WebView
+        source={{ uri: 'http://localhost:3000' }}
+        style={{ width: "100%" }}
+        injectedJavaScriptBeforeContentLoaded={INJECTED_JAVASCRIPT}
+        onMessage={(event) => {
+          event.nativeEvent.data
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +30,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
